e1a277dd96374185aeb4d33d1dba4864a29e2afb
andzone <stoneworld1991@gmail.com>
2022-02-27 17:21:38 +0800

    token

---
+++ W1/scripts/TokenCreator.js
@@ -0,0 +1,39 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+const web3 = require("web3-utils");
+
+
+async function main() {
+    // Hardhat always runs the compile task when running scripts with its command
+    // line interface.
+    //
+    // If this script is run directly using `node` you may want to call compile
+    // manually to make sure everything is compiled
+    // await hre.run('compile');
+
+    // We get the contract to deploy
+    const TokenCreator = await hre.ethers.getContractFactory("TokenCreator");
+    const creator = await TokenCreator.deploy();
+
+    await creator.deployed();
+
+    console.log("TokenCreator deployed to:", creator.address);
+
+    const bbsAddress = await creator.createToken(web3.randomHex(32));
+
+    console.log("bbaAddress: ", bbsAddress);
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
