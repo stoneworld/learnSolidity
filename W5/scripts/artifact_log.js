6a897c901617d0d914cccdd393f28e4f77fac13c
汪帅 <wangshuai1@soyoung.com>
2022-03-29 11:50:43 +0800

    w5

--- W5/scripts/artifact_log.js
+++ W5/scripts/artifact_log.js
@@ -4,7 +4,6 @@ const util = require('util');
 
 const writeFile = util.promisify(fs.writeFile);
 
-
 async function writeAddr(addr, name, network) {
     const deployments = {};
     deployments["address"] = addr;
@@ -12,10 +11,41 @@ async function writeAddr(addr, name, network) {
     await writeLog(deployments, name, network);
 }
 
-// for Hardhat deployment
 async function writeAbiAddr(artifacts, addr, name, network) {
+    console.log(`../deployments/${network}/${name}.json`)
     const deployments = {};
     deployments["address"] = addr;
+    deployments["contractName"] = artifacts.contractName;
+
+    var dir = './deployments/abi';
+
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+
+    var dir = './deployments/dev';
+
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+
+
+    const deploymentDevPath = path.resolve(__dirname, `../deployments/dev/${network}-${deployments["contractName"]}.json`);
+    await writeFile(deploymentDevPath, JSON.stringify(deployments, null, 2));
+
+    const abis = {};
+    abis["contractName"] = artifacts.contractName;
+    abis["abi"] = artifacts.abi;
+    const deploymentAbiPath = path.resolve(__dirname, `../deployments/abi/${abis["contractName"]}.json`);
+
+
+    await writeFile(deploymentAbiPath, JSON.stringify(abis, null, 2));
+}
+
+// for Truffle deployment
+async function writeAbis(artifacts, name, network) {
+    const deployments = {};
+    deployments["address"] = artifacts.address;
     deployments["contractName"] = artifacts.contractName;
     await writeLog(deployments, name, network);
 
@@ -26,7 +56,6 @@ async function writeAbiAddr(artifacts, addr, name, network) {
     const deploymentPath = path.resolve(__dirname, `../deployments/abi/${abis["contractName"]}.json`);
     await writeFile(deploymentPath, JSON.stringify(abis, null, 2));
 }
-
 
 /**
  * 记录合约发布地址
@@ -42,6 +71,7 @@ async function writeLog(deployments, name, network) {
 
 module.exports = {
     writeLog,
+    writeAbis,
     writeAbiAddr,
     writeAddr
 }
